4092e927c0382a08e077a0d4a346596a2970e261
Sternee <sternee2332006@gmail.com>
2025-09-17 11:03:05 +0530

    Development commit #3

--- rcnb/build.sh
+++ rcnb/build.sh
@@ -10,5 +10,20 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Apply database migrations
-python manage.py makemigrations
+# It's good practice to migrate each app individually
+# to ensure all dependencies are met.
+python manage.py migrate auth
+python manage.py migrate contenttypes
+python manage.py migrate sessions
+python manage.py migrate admin
+python manage.py migrate home
+python manage.py migrate startups
+python manage.py migrate products
+python manage.py migrate orders
+python manage.py migrate events
+python manage.py migrate favorites
+python manage.py migrate cart
+python manage.py migrate users
+
+# Finally, run migrate again to catch any remaining dependencies
 python manage.py migrate
